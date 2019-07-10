199d258e1b64d2f1d5a86385d5c5c723cd00d23f
Fargekritt <lullinj98@gmail.com>
2019-07-10 08:20:49 +0200

    Updated font and title in GUI

--- pwndGUI.py
+++ pwndGUI.py
@@ -3,6 +3,8 @@ import pwnd
 
 HEIGHT = 700
 WIDTH = 900
+window_title = "have you been pwned?"
+font_size = "Helvetica 15"
 
 
 def button_click(entry):
@@ -15,6 +17,7 @@ def button_click(entry):
 
 
 root = tk.Tk()
+root.title(window_title)
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
@@ -23,10 +26,10 @@ frame = tk.Frame(root, bg="blue", bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75,
             relheight=0.1, anchor="n")
 
-entry = tk.Entry(frame,)
+entry = tk.Entry(frame, font=font_size)
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="button",
+button = tk.Button(frame, text="Check password", font=font_size,
                    command=lambda: button_click(entry.get()))
 button.place(relx=0.7, relwidth=0.3, relheight=1)
 
@@ -35,7 +38,7 @@ lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75,
                   relheight=0.6, anchor="n")
 
 label = tk.Label(lower_frame,
-                 anchor="nw", justify="left", bd=4)
+                 anchor="nw", justify="left", bd=4, font=font_size)
 label.place(relwidth=1, relheight=1)
 
 root.mainloop()
